9d92617f85affac79521b414b715ceae3ba0879f
tiffanybacher <tiffanybacher@gmail.com>
2019-07-01 17:31:48 -0600

    Create GET endpoint for a palette
    Co-Authored-By: BrennanDuffey <brennanduffey@gmail.com>

--- server.js
+++ server.js
@@ -21,6 +21,22 @@ app.get('/api/v1/palettes', (request, response) => {
     });
 });
 
+app.get('/api/v1/palettes/:id', (request, response) => {
+  const id = request.params.id;
+
+  database('palettes').where({ id }).select()
+    .then(palette => {
+      if (palette.length) {
+        response.status(200).json(palette[0]);
+      } else {
+        response.status(404).json({ error: `A palette with id of ${id} was not found.` });
+      }
+    })
+    .catch(error => {
+      response.status(500).json({ error });
+    });
+});
+
 app.get('/api/v1/projects', (request, response) => {
   database('projects').select()
     .then(projects => {
@@ -30,4 +46,3 @@ app.get('/api/v1/projects', (request, response) => {
       response.status(500).json({ error });
     });
 });
-
