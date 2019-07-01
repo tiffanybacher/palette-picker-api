321f7fb90714ae4a04e753fd31c67efd784c6b25
tiffanybacher <tiffanybacher@gmail.com>
2019-07-01 17:14:28 -0600

    Create server.js with palettes GET endpoint
    Co-Authored-By: BrennanDuffey <brennanduffey@gmail.com>

---
+++ server.js
@@ -0,0 +1,22 @@
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
+const port = 3001;
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.listen(process.env.PORT || port, () => {
+  console.log(`Palette Picker is running on ${port}.`);
+});
+
+app.get('/api/v1/palettes', (request, response) => {
+  database('palettes').select()
+    .then(palettes => {
+      response.status(200).json(palettes);
+    })
+    .catch(error => {
+      response.status(500).json({ error });
+    });
+});
