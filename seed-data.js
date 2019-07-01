40826bfb9b96c57a71cc369f3cb3f175c0d2cf14
tiffanybacher <tiffanybacher@gmail.com>
2019-07-01 16:30:03 -0600

    Create seed data
    Co-Authored-By: BrennanDuffey <brennanduffey@gmail.com>

---
+++ seed-data.js
@@ -0,0 +1,28 @@
+const projects = [
+  { 
+    name: 'Band Poster',
+    palette: {
+      name: 'Reggae Colors',
+      colors: [
+        '5F0F40',
+        '9A031E',
+        'FB8B24',
+        'E36414',
+        '0F4C5C'
+      ]
+    }
+  },
+  { 
+    name: 'Website',
+    palette: {
+      name: 'Website Colors',
+      colors: [
+        '495867',
+        '577399',
+        'BDD5EA',
+        'F7F7FF',
+        'FE5F55'
+      ]
+    }
+  }
+]
